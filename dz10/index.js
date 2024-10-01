dba382017fb19d9ac3889bb86b4047fcd44f22e4
KuKriK <ilapolityko@gmail.com>
2024-10-01 22:01:47 +0300

    Add dz10

---
+++ dz10/index.js
@@ -0,0 +1,21 @@
+function sum(one, two){
+  const result = one + two
+  return result
+}
+function divide(one, two){
+  const result = one / two
+  return result
+}
+function subtract(one, two){
+  const result = one - two
+  return result
+}
+
+
+let result = sum(4,6)
+console.log(result)
+result = divide(result,2)
+console.log(result)
+result = subtract(result,3)
+console.log(result)
+
