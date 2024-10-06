2c7bd9297477b5addbc58adad75c2cd5d762ff7b
KuKriK <ilapolityko@gmail.com>
2024-10-06 15:29:37 +0300

    Real install eslint

--- dz11/src/app.js
+++ dz11/src/app.js
@@ -1,39 +1,34 @@
-function Rever(str){
-return str.split("").reverse().join("")
+function Rever(str) {
+  return str.split('').reverse().join('')
 }
-
-function Find(str, substring){
-  if(str.indexOf(substring) !== -1){
+function Find(str, substring) {
+  if (str.indexOf(substring) !== -1) {
     return true
   }
-  else{
-    return false
+  return false
+}
+function ReverForMass(mass) {
+  for (let i = mass.length - 1; i != -1; i--) {
+    console.log(mass[i])
   }
 }
-function ReverForMass(mass){
-   for(let i = mass.length -1; i !=-1; i--){
-    console.log(mass[i])
-   }
-}
-
-function main(){
-  let str = "Hello World"
-  console.log('Длина ' + str.length);
+function main() {
+  let str = 'Hello World'
+  console.log(`Длина ${str.length}`)
   str = Rever(str)
-  console.log('Реверс ' + str)
-  console.log('Содержит подстроку '+ Find(str, 'He'))
-  let mass = ['string', 5]
-  console.log('Количество элементов ' + mass.length)
+  console.log(`Реверс ${str}`)
+  console.log(`Содержит подстроку ${Find(str, 'He')}`)
+  const mass = ['string', 5]
+  console.log(`Количество элементов ${mass.length}`)
   mass.push(true)
   mass.unshift('c++ gadost')
-  console.log('Новое количество элементов ' + mass.length)
-  while(mass.length > 2){
-    mass.pop();
+  console.log(`Новое количество элементов ${mass.length}`)
+  while (mass.length > 2) {
+    mass.pop()
   }
-  for(el of mass){
+  for (el of mass) {
     console.log(el)
   }
   ReverForMass(mass)
 }
-
 main()
