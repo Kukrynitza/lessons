01b883f48343d773991237000acb1e10e8b432ad
KuKriK <ilapolityko@gmail.com>
2024-10-31 01:26:41 +0300

    Add dz18

---
+++ dz18/src/app.js
@@ -0,0 +1,5 @@
+import sum from './sum.js'
+console.log(sum(3, 4))
+import { a, sumString } from './sum-string.js'
+console.log(sumString(5, 9))
+console.log(a)
---
+++ dz18/src/sum-string.js
@@ -0,0 +1,5 @@
+export function sumString(a, b) {
+  return `${a} + ${b}`
+}
+
+export const a = 6
