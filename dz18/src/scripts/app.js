779fd5decc3dabc458b3f648fe41a62c4a285776
KuKriK <ilapolityko@gmail.com>
2024-11-02 01:55:27 +0300

    Correct dz18(without validation)

--- dz18/src/scripts/app.js
+++ dz18/src/scripts/app.js
@@ -1,7 +1,21 @@
-function createUser(event) {
+async function createUser(event) {
   event.preventDefault()
-  console.log(event.target)
+  const email = event.target.querySelector('#email').value
+  const userPassword = event.target.querySelector('#password').value
+  console.log(email, userPassword)
+  const response = await fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+
+      username: email,
+      password: userPassword
+    })
+  })
+  const { refreshToken } = await response.json()
+  console.log(refreshToken)
+  window.localStorage.setItem('refreshToken', refreshToken)
 }
 
-const userSubmit = document.querySelectorAll('.user')
+const userSubmit = document.querySelector('.center-form')
 userSubmit.addEventListener('submit', createUser)
