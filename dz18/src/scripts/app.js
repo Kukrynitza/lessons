fddcad1d989915163b9d951942a5cae713a460ac
KuKriK <ilapolityko@gmail.com>
2024-11-03 01:14:26 +0300

    Correct dz18

--- dz18/src/scripts/app.js
+++ dz18/src/scripts/app.js
@@ -3,31 +3,15 @@ async function createUser(event) {
   const email = event.target.querySelector('#email').value
   const userPassword = event.target.querySelector('#password').value
   if (email.length < 3) {
-    document.querySelector('.login').innerHTML = `
-    <label for="email">Email</label>
-    <div>
-    <input id="email" name="email" placeholder="Enter your email" required autocomplete autofocus>
-    <p class="error">Error email length < 3</p>
-    </div>
-    <label for="password">Password</label>
-    <input id="password" name="password" placeholder="Create a password" required autocomplete>`
+    document.querySelector('.login').appendChild('<p class="error">Error email length < 3</p>')
 
     return
   }
   if (userPassword.length < 6) {
-    document.querySelector('.login').innerHTML = `
-      <label for="email">Email</label>
-      <input id="email" name="email" placeholder="Enter your email" required autocomplete autofocus>
-      <label for="password">Password</label>
-      <div>
-        <input id="password" name="password" placeholder="Create a password" required autocomplete>
-        <p class="error">Error: password length must be at least 6 characters</p>
-      </div>
-    `
+    document.querySelector('.login').appendChild('Error: password length must be at least 6 characters')
 
     return
   }
-  document.querySelector('.login').innerHTML = '<label for="email">Email</label><input id="email" name="email" placeholder="Enter your email" required autocomplete autofocus><label for="password">Password</label><input id="password" name="password" placeholder="Create a password" required autocomplete></input>'
   console.log(email, userPassword)
   const response = await fetch('https://dummyjson.com/auth/login', {
     method: 'POST',
@@ -40,7 +24,7 @@ async function createUser(event) {
   })
   const { refreshToken } = await response.json()
   console.log(refreshToken)
-  window.localStorage.setItem('refreshToken', refreshToken)
+  localStorage.setItem('refreshToken', refreshToken)
 }
 
 const userSubmit = document.querySelector('.center-form')
