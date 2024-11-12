9ead8d9c301a2e7b9e73e4d3b513467edb0251b3
KuKriK <ilapolityko@gmail.com>
2024-11-12 23:56:32 +0300

    Correct dz20

--- dz20/index.jsx
+++ dz20/index.jsx
@@ -3,5 +3,5 @@ import 'the-new-css-reset/css/reset.css'
 import App from './src/App.jsx'
 import './src/app.css'
 
-const root = createRoot(document.querySelector('.app'))
+const root = createRoot(document.querySelector('#app'))
 root.render(<App />)
