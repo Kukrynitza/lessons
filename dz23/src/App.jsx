95d6e79de53a82ee70de0f816dba3626dd5970a5
KuKriK <ilapolityko@gmail.com>
2024-11-22 13:30:37 +0300

    Correct UsersList

--- dz23/src/App.jsx
+++ dz23/src/App.jsx
@@ -1,7 +1,11 @@
 import Recipes from './components/Recipes/Recipes.jsx'
+import UserList from './components/UsersList/UsersList.jsx'
 
 export default function App() {
   return (
-    <Recipes />
+    <>
+      <Recipes />
+      <UserList />
+    </>
   )
 }
--- dz23/src/components/UsersList/GetInstruction.jsx
+++ dz23/src/components/UsersList/GetInstruction.jsx
@@ -17,7 +17,7 @@ export default function GetInstruction({ user }) {
   }, [user.id])
   console.log(data)
   if (!data) {
-    return <p>Loading...</p>
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 150"><path fill="none" stroke="#4E9173" strokeWidth="15" strokeLinecap="round" strokeDasharray="300 385" strokeDashoffset="0" d="M275 75c0 31-27 50-50 50-58 0-92-100-150-100-28 0-50 22-50 50s23 50 50 50c58 0 92-100 150-100 24 0 50 19 50 50Z"><animate attributeName="stroke-dashoffset" calcMode="spline" dur="2" values="685;-685" keySplines="0 0 1 1" repeatCount="indefinite"></animate></path></svg>
   }
 
   return (
