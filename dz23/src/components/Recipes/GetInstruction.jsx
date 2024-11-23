ffba09af452a221b7096761d74dee570a3e1383f
KuKriK <ilapolityko@gmail.com>
2024-11-23 12:23:03 +0300

    Correct dz23

--- dz23/src/components/Recipes/GetInstruction.jsx
+++ dz23/src/components/Recipes/GetInstruction.jsx
@@ -1,23 +1,35 @@
+import delay from 'delay'
 import { useContext, useEffect, useState } from 'react'
 import useSWR from 'swr'
 import styles from './Recipes.module.css'
 
 export default function GetInstruction({ title }) {
   const fetcher = async (get) => {
+    await delay(2000)
     const response = await fetch(get)
 
     return response.json()
   }
   const { data, error, isLoading } = useSWR(`https://dummyjson.com/recipes/search?q=${title}`, fetcher)
   if (error) {
-    return (<div>failed to load</div>)
+    return (<p>failed to load</p>)
   }
   if (isLoading) {
-    return (<div>loading...</div>)
+    return (<p className={styles.loader}>loading
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+        <circle fill="#24344A" stroke="#24344A" strokeWidth="15" r="15" cx="40" cy="100">
+          <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#24344A" stroke="#24344A" strokeWidth="15" r="15" cx="100" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#24344A" stroke="#24344A" strokeWidth="15" r="15" cx="160" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0">
+        </animate>
+        </circle>
+      </svg>
+    </p>)
+  }
+  if (!data.recipes[0]) {
+    return (<p >not fount</p>)
   }
 
   return (
-    <ul>
+    <ul className={styles.ul}>
       {data.recipes[0]?.instructions.map((oneInstruction, index) => (
         <li key={index}>{oneInstruction}</li>
       ))}
@@ -29,27 +41,26 @@ export default function GetInstruction({ title }) {
 // import useSWR from 'swr'
 // import styles from './Recipes.module.css'
 
-// export default function GetInstruction(title) {
-//   const fetcher = async (url) => {
-//     const response = await fetch(url)
+// export default function GetInstruction({ title }) {
+//   const [instruction, setInstruction] = useState(null)
+//   const fetcher = async (get) => {
+//     const response = await fetch(get)
 
 //     return response.json()
 //   }
-//   const [instruction, setInstruction] = useState(null)
-//   const { data, error, isLoading } = useSWR(https://dummyjson.com/recipes/search?q=${title}, fetcher)
+//   const { data, error, isLoading } = useSWR(`https://dummyjson.com/recipes/search?q=${title}`, fetcher)
 //   if (error) {
 //     return (<div>failed to load</div>)
 //   }
 //   if (isLoading) {
 //     return (<div>loading...</div>)
 //   }
-//   setInstruction(data.instructions)
-//   console.log(isLoading)
+//   setInstruction(data)
 
 //   return (
 //     <ul>
-//       {instruction.map((oneInstruction) => (
-//         <li key={oneInstruction}>{oneInstruction}</li>
+//       {data.recipes[0]?.instructions.map((oneInstruction, index) => (
+//         <li key={index}>{oneInstruction}</li>
 //       ))}
 //     </ul>
 //   )
--- dz23/src/components/Recipes/Recipes.jsx
+++ dz23/src/components/Recipes/Recipes.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { useEffect, useState } from 'react'
 import GetInstruction from './GetInstruction.jsx'
 import styles from './Recipes.module.css'
@@ -23,10 +24,15 @@ export default function Recipes() {
   }
 
   return (
-    <ul>
+    <ul className={styles.mainUl}>
       {recipesList.map(({ on, title }) => (
-        <li key={title} className={styles.column}>
-          <button onClick={() => handleClick(title)}>{title}</button>
+        <li key={title} className={on
+          ? classNames(styles.column, styles.columnOn)
+          : classNames(styles.column, styles.columnOff)}>
+          <button className={on
+            ? classNames(styles.recipe, styles.onRecipe)
+            : classNames(styles.recipe, styles.offRecipe)}
+          onClick={() => handleClick(title)}>{title}</button>
           {on ? <GetInstruction title = {title}/> : <p>Click on user</p>}
         </li>
       ))}
