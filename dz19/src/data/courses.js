10c8c3171ec57849e61ab2451f2692ca59ad4aa1
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-25 18:09:11 +0300

    Move courses to data folder

--- dz19/src/data/courses.js
+++ dz19/src/data/courses.js
@@ -1,32 +1,35 @@
 export default [
   {
-    title: 'Курсы компании «Гарцующий пони»',
     author: 'Иван Иванович',
+    date: '13 февраля 2023',
     image: {
-      src: 'https://doka.guide/html/semantics/demos/bad-markup/images/seo-course.png',
-      alt: 'Ключевое слово SEO'
+      alt: 'Ключевое слово SEO',
+      src: 'https://doka.guide/html/semantics/demos/bad-markup/images/seo-course.png'
     },
     info: 'Благодаря этому курсу вы научитесь задавливать конкурентов с помощью ссылочной массы, а не качественного контента.',
-    date: '13 февраля 2023'
+    slug: 'seo',
+    title: 'SEO для начинающих'
   },
   {
-    title: 'WordPress разработка',
     author: 'Ольга Ольгина',
+    date: '23 октября 2023',
     image: {
-      src: 'https://doka.guide/html/semantics/demos/bad-markup/images/wordpress-course.png',
-      alt: 'Ключевое слово SEO'
+      alt: 'Ключевое слово WordPress',
+      src: 'https://doka.guide/html/semantics/demos/bad-markup/images/wordpress-course.png'
     },
     info: 'WordPress — топ за свои деньги. Изучите его, чтобы стать востребованным фрилансером.',
-    date: '23 октября 2023'
+    slug: 'wordpress',
+    title: 'WordPress разработка'
   },
   {
-    title: 'JavaScript для чайников',
     author: 'М. Михайловских',
+    date: '30 ноября 2023',
     image: {
-      src: 'https://doka.guide/html/semantics/demos/bad-markup/images/javascript-course.png',
-      alt: 'Ключевое слово SEO'
+      alt: 'Ключевое слово JavaScript',
+      src: 'https://doka.guide/html/semantics/demos/bad-markup/images/javascript-course.png'
     },
     info: ' Курс подойдёт для любых чайников: электрических, газовых и даже для кастрюлек, временно подменяющих сломанный чайник.',
-    date: '30 ноября 2023'
+    slug: 'javascript',
+    title: 'JavaScript для чайников'
   }
 ]
