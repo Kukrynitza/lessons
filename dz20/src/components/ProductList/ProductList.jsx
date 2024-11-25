d048d5e2f2b2e22719c8a45ea265dd9ac2a3869f
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-25 19:39:02 +0300

    Fix indentation, simplify styling

--- dz20/src/components/ProductList/ProductList.jsx
+++ dz20/src/components/ProductList/ProductList.jsx
@@ -1,21 +1,20 @@
 import products from '../../data/Products.jsx'
 import styles from './ProductList.module.css'
+
 export default function ProductList() {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id} className={styles.li}>
+        <li key={product.id} className={styles.product}>
           <p>{product.id}</p>
           <p>{product.title}</p>
-          <p>{product.discount > 0
-            ? (
-              <>
-                <span>%</span> {product.price * (1 - (product.discount / 100))}
-              </>
-            )
-            : product.price
-          }</p>
-          <img src={product.image} alt={product.title} />
+          <p className={styles.price}>
+            {product.discount > 0 ? (
+              <><span>%</span> {product.price * (1 - (product.discount / 100))}</>
+            ) : (
+              product.price
+            )}
+          </p>
         </li>
       ))}
     </ul>
